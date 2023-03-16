40b6d7e1a0109129479750904425a445d80a3cf2
Sysadmin John <61210792+sysadminjohn@users.noreply.github.com>
2023-03-16 13:31:51 +0100

    Remove dependency on sed, use alternate method (mapfile), add cleanup function

--- linux-random.sh
+++ linux-random.sh
@@ -4,28 +4,42 @@
 
 clear
 
+###
+function cleanup {
+    rm randomized-question-pool.txt 2>/dev/null
+	unset randomizedquestionpool
+}
+trap cleanup EXIT
+###
+
+###
 which shuf 2>/dev/null 1>/dev/null
 
 if [[ $? -ne 0 ]] ; then
 	echo "No shuf command found. It's usually part of Linux coreutils package"
 	exit 1
 fi
+###
 
-
+###
 if [[ -z "$1" ]] ; then	
 
-	rm .randomized-question-pool.txt 2>/dev/null
-	shuf question-pool.txt > .randomized-question-pool.txt
-	
-	QUESTIONS_REMAINING=$(wc -l .randomized-question-pool.txt | awk '{ print $1 }')
-	QUESTION_NUMBER=1
 
-	while [[ QUESTIONS_REMAINING -ge 1 ]] ; 
+	rm randomized-question-pool.txt 2>/dev/null
+	shuf question-pool.txt > randomized-question-pool.txt
+	mapfile -t randomizedquestionpool < randomized-question-pool.txt
+
+	QUESTIONS_REMAINING=$(wc -l randomized-question-pool.txt | awk '{ print $1 }')
+	QUESTION_NUMBER=0
+
+	rm randomized-question-pool.txt 2>/dev/null
+	
+	while [[ QUESTIONS_REMAINING -gt 0 ]] ; 
 		do
 			clear
 			echo "_______________________________________________________________________"
 			echo ""
-			echo $(sed -n "$QUESTION_NUMBER"'p' .randomized-question-pool.txt)
+			echo "${randomizedquestionpool[$QUESTION_NUMBER]}"
 			echo ""
 			echo "_______________________________________________________________________"
 			echo "[$QUESTIONS_REMAINING questions left]"
@@ -37,7 +51,6 @@ if [[ -z "$1" ]] ; then
 			
 		done 
 
-	rm .randomized-question-pool.txt 2>/dev/null
 	
 	echo "============================================================"
 	echo "DONE: All questions cycled. Re-run the program to go at it again with a newly randomized list"
@@ -58,3 +71,4 @@ else
 
 	exit 0
 fi
+###
